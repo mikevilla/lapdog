ce4955ea0c7e14e76725064ae9af60c13cdfe9f3
Bibby <mikevilla@gmail.com>
2014-10-07 14:10:10 -0700

    add error checking to the sign the petition form

--- js/can/lapdog_page_controller.js
+++ js/can/lapdog_page_controller.js
@@ -88,6 +88,44 @@ var lapdogPageControl  = can.Control(
 
       }, null);
 
+
+
+      $( "#sign-petition" ).click(function( event ) {
+
+        // Stop form from submitting normally
+        event.preventDefault();
+        console.log("sign-petition");
+
+        // extract values from the form
+        var $form = $("#signup"),
+        email = $form.find("#email").val(),
+        firstname = $form.find("#firstname").val(),
+        lastname = $form.find("#lastname").val(),
+        zip = $form.find("#zip").val(),
+        isEmailValid = false;
+
+
+        console.log("email", email);
+        console.log("firstname", firstname);
+        console.log("lastname", lastname);
+        console.log("zip", zip);
+
+        if ( !outer.isValidEmailAddress( email ) ) {
+         /* do stuff here */
+          isEmailValid = false;
+
+          // display the error message
+          $(".email-error").css('visibility', 'visible');
+          console.log("email is invalid")
+        } else {
+          isEmailValid = true;
+          console.log("VALID EMAIL")
+        }
+
+        outer.checkform($form);
+
+      });
+
       this.set_up_complete = true;
     }
   },
@@ -97,9 +135,30 @@ var lapdogPageControl  = can.Control(
       base = base[ names[i] ] = base[ names[i] ] || {};
     }
 
+  },
+
+  isValidEmailAddress : function(emailAddress) {
+      var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+      return pattern.test(emailAddress);
+  },
+
+  checkform :function (form) {
+      // clear the error messages
+      $(".error-msg").css('visibility', 'hidden');
+
+
+      var inputs = $('input');
+      for (var i = 0; i < inputs.length; i++) {
+          // only validate the inputs that have the required attribute
+          if(inputs[i].hasAttribute("required")){
+              if(inputs[i].value == ""){
+                  // found an empty field that is required
+                  $(inputs[i]).next().css('visibility', 'visible');
+              }
+          }
+      }
+      return true;
   }
-
-
 
 
 });
